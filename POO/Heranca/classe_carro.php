33727b7f6d839f0d32cf1bdf8c12644a62657311
dayvison06 <dayvison.rocha@hotmail.com>
2024-07-01 15:25:27 -0300

    Modificadores de acesso (Public, protecte, private)

--- POO/Heranca/classe_carro.php
+++ POO/Heranca/classe_carro.php
@@ -2,8 +2,10 @@
 
 class Veiculo
 {
-    public $modelo;
-    public $cor;
+    //Quando protected só é acessivel por métodos da propria classe ou de classe herdeira
+    protected $modelo;
+    //Quando private, só é acessível por métodos da propria clase.
+    private $cor;
     public $ano;
 
     public function Andar()
@@ -15,6 +17,15 @@ class Veiculo
     {
         echo "Parou";
     }
+
+    public function getModelo(){
+        return $this->modelo;    
+    }
+
+    public function setModelo($modelo){
+        $this->modelo = $modelo;
+    }
+
 }
 
 // Classe Carro herdado atributos e métodos de Veiculo
@@ -24,6 +35,14 @@ class Carro extends Veiculo
     public function ligarLimpador()
     {
         echo "Limpando...";
+    }
+
+    public function getCor(){
+        return $this->cor;    
+    }
+
+    public function setCor($cor){
+        $this->cor = $cor;
     }
 }
 
@@ -41,8 +60,10 @@ class Moto extends Veiculo
 
 // Instancia da Classe Carro
 $carro = new Carro();
-$carro->modelo = "Gol";
-$carro->cor = "Vermelho";
+// $carro->modelo = "Gol";
+$carro->setModelo("Gol");
+// $carro->cor = "Vermelho";
+$carro->setCor("Amarelo");
 $carro->ano = 2008;
 $carro->Andar();
 echo "<br>";
@@ -51,7 +72,8 @@ var_dump($carro);
 
 //Instancia da Classe Moto
 $moto = new Moto();
-$moto->modelo = "Honda";
+// $moto->modelo = "Honda";
+$moto->setModelo("Honda");
 $moto->cor = "Azul";
 $moto->ano = 2019;
 $moto->Parar();
